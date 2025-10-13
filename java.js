922cf777e4cd11dc3222c531cface60220c7b6d4
Andre Martins <andremartins2803@gmail.com>
2025-10-12 22:51:43 -0300

    teste da segunda pag

--- java.js
+++ java.js
@@ -17,3 +17,26 @@ document.getElementById("S_dentista").style.display = "none";
         document.getElementById("S_emocional").style.display = "none";
     }
 }
+function exibir_T(){
+    if(T_geral.style.display === "none"){
+document.getElementById("T_geral").style.display = "block";
+document.getElementById("T_aposentadoria").style.display = "none";
+    }else{
+        document.getElementById("T_geral").style.display = "none";
+    }
+}
+function exibir_A(){
+    if(T_aposentadoria.style.display === "none"){
+document.getElementById("T_geral").style.display = "none";
+document.getElementById("T_aposentadoria").style.display = "block";
+    }else{
+        document.getElementById("T_aposetadoria").style.display = "none";
+    }
+}
+function exibir_Edu(){
+    if(Edu.style.display === "none"){
+document.getElementById("Edu").style.display = "block";
+    }else{
+        document.getElementById("Edu").style.display = "none";
+    }
+}
