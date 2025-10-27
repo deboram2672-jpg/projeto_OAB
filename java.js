5a868abb93539519cc6d8eb42b0337b5710b3913
deboram2672-jpg <debora.m2672@gmail.com>
2025-10-27 09:54:47 -0300

    Alteracao
    Arrumando abas

--- java.js
+++ java.js
@@ -1,63 +1,26 @@
-function exibir_G(){
-    if(S_geral.style.display === "none"){
-document.getElementById("S_geral").style.display = "block";
-document.getElementById("S_emocional").style.display = "none";
-document.getElementById("S_dentista").style.display = "none";
+const todosConteudos = [
+  "S_geral",
+  "S_emocional",
+  "S_dentista",
+  "T_geral",
+  "T_aposentadoria",
+  "Edu",
+  "Qm_somos",
+  "Sobre_pj",
+  "jogo_regras"
+];
 
-    }else{
-        document.getElementById("S_geral").style.display = "none";
+function exibirConteudo(idParaExibir) {
+  const elemento = document.getElementById(idParaExibir);
+  const estaVisivel = elemento.style.display === "block";
+
+  todosConteudos.forEach(id => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.style.display = "none";
     }
-}
-function exibir_E(){
-    if(S_emocional.style.display === "none"){
-document.getElementById("S_geral").style.display = "none";
-document.getElementById("S_emocional").style.display = "block";
-document.getElementById("S_dentista").style.display = "none";
-    }else{
-        document.getElementById("S_emocional").style.display = "none";
-    }
-}
-function exibir_T(){
-    if(T_geral.style.display === "none"){
-document.getElementById("T_geral").style.display = "block";
-document.getElementById("T_aposentadoria").style.display = "none";
-    }else{
-        document.getElementById("T_geral").style.display = "none";
-    }
-}
-function exibir_A(){
-    if(T_aposentadoria.style.display === "none"){
-document.getElementById("T_geral").style.display = "none";
-document.getElementById("T_aposentadoria").style.display = "block";
-    }else{
-        document.getElementById("T_aposetadoria").style.display = "none";
-    }
-}
-function exibir_Edu(){
-    if(Edu.style.display === "none"){
-document.getElementById("Edu").style.display = "block";
-    }else{
-        document.getElementById("Edu").style.display = "none";
-    }
-}
-function exibir_QM(){
-    if(Qm_somos.style.display === "none"){
-document.getElementById("Qm_somos").style.display = "block";
-    }else{
-        document.getElementById("Qm_somos").style.display = "none";
-    }
-}
-function exibir_GP(){
-    if(Sobre_pj.style.display === "none"){
-document.getElementById("Sobre_pj").style.display = "block";
-    }else{
-        document.getElementById("Sobre_pj").style.display = "none";
-    }
-}
-function exibir_JG(){
-    if(jogo_regras.style.display === "none"){
-document.getElementById("jogo_regras").style.display = "block";
-    }else{
-        document.getElementById("jogo_regras").style.display = "none";
-    }
+  });
+  if (!estaVisivel) {
+    elemento.style.display = "block";
+  }
 }
